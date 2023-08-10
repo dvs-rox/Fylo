a11d80b0671ecbc5744314079a4a826a1dfeefa2
Yona <dvsscheme@gmail.com>
2023-08-10 15:55:32 +0300

    finalization

---
+++ src/components/EarlyAccess.vue
@@ -0,0 +1,12 @@
+<template>
+    <section class="early-access">
+        <h1>Get early access today</h1>
+        <p>
+            It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions, our support team would be happy to help you.
+        </p>
+        <div>
+            <input type="text">
+            <button class="get-started-button">Get started for free</button>
+        </div>
+    </section>
+</template>
